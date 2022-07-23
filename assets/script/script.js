4694ee7f19484f3981282d4e5f2050f97f8b6f07
ZahraMousavi612 <mousavizahra612@gamil.com>
2022-07-23 21:30:18 +0000

    select all reqired elements

---
+++ assets/script/script.js
@@ -0,0 +1,14 @@
+// Selecting all required elements
+const selectBox = document.querySelector(".select_box"),
+ selectXBtn = selectBox.querySelector(".playerX"),
+ selectOBtn = selectBox.querySelector(".playerO"),
+ playBoard = document.querySelector(".play_board"),
+ allBox = document.querySelectorAll("section span"),
+ players = document.querySelector(".players"),
+ resultBox = document.querySelector(".result_box"),
+ wonText = resultBox.querySelector(".won_text");
+ replayBtn = resultBox.querySelector("button");
+var voice = document.getElementById('audio');
+ audio-icon= document.getElementById('audio-icon');
+ light-icon=document.getElementById('light-icon');
+
